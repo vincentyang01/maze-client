52fc33b9a518679258fd47f6b3d83e3946eac016
vincentyang01 <vincentyangcsi@gmail.com>
2020-09-03 21:11:29 -0400

    Missed a save on inital commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Runtime
+
+    const mazeFetchAdapter = new MazeFetchAdapter("http://localhost:3000/")
+
+    //Fetch all users and log their names
+
+    const action = users => users.forEach(console.log)
+
+    mazeFetchAdapter.get("maze", action)
+})
