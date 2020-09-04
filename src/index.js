73df89cb00afffdba81feb60d82da232110a43f4
Jake Mills <jake.mills.05@gmail.com>
2020-09-04 15:51:06 -0400

    Added maze generation algorithm

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Runtime
 
-    const mazeFetchAdapter = new MazeFetchAdapter("http://localhost:3000/")
+    // const mazeFetchAdapter = new MazeFetchAdapter("http://localhost:3000/")
 
     //Fetch all users and log their names
 
     const action = users => users.forEach(console.log)
 
-    mazeFetchAdapter.get("maze", action)
+    // mazeFetchAdapter.get("maze", action)
 })
