7b7571cf448c23a2479e9a1ec424f3670f58a887
Jake Mills <jake.mills.05@gmail.com>
2020-09-08 10:04:58 -0400

    File for creating grid/cells

--- src/cell.js
+++ src/cell.js
@@ -1,66 +1,75 @@
-function Cell(i, j) {
-  this.i = i;
-  this.j = j;
-  this.walls = [true, true, true, true];
-  this.visited = false;
+const rows = 15
+const columns = 15
+let grid = [];
 
-  this.checkNeighbors = function () {
-    let neighbors = [];
+class Cell {
+  constructor(x, y) {
+    this.row = x;
+    this.column = y;
+    this.walls = [true, true, true, true];
+    this.visited = false;
+    this.neighbors = [];
+  }
 
-    let top = grid[index(i, j - 1)];
-    let right = grid[index(i + 1, j)];
-    let bottom = grid[index(i, j + 1)];
-    let left = grid[index(i - 1, j)];
+  findNeighbors() {
+    if (this.row > 0) {
+      this.neighbors.push(grid[(this.row - 1) * columns + this.column]);
+    }
+    if (this.column < columns - 1) {
+      this.neighbors.push(grid[this.row * columns + (this.column + 1)]);
+    }
+    if (this.row < rows - 1) {
+      this.neighbors.push(grid[(this.row + 1) * columns + this.column]);
+    }
+    if (this.column > 0) {
+      this.neighbors.push(grid[this.row * columns + (this.column - 1)])
+    };
+  }
 
-    if (top && !top.visited) {
-      neighbors.push(top);
-    }
-    if (right && !right.visited) {
-      neighbors.push(right);
-    }
-    if (bottom && !bottom.visited) {
-      neighbors.push(bottom);
-    }
-    if (left && !left.visited) {
-      neighbors.push(left);
-    }
+  show() {
+    if (!this.walls[0])
+      cells[this.row * columns + this.column].style["border-top"] = "none";
+    if (!this.walls[1])
+      cells[this.row * columns + this.column].style["border-right"] = "none";
+    if (!this.walls[2])
+      cells[this.row * columns + this.column].style["border-bottom"] = "none";
+    if (!this.walls[3])
+      cells[this.row * columns + this.column].style["border-left"] = "none";
 
-    if (neighbors.length > 0) {
-      let r = floor(random(0, neighbors.length));
-      return neighbors[r];
-    } else {
-      return undefined;
-    }
-  };
-  this.highlight = function () {
-    let x = this.i * w;
-    let y = this.j * w;
-    noStroke();
-    fill(0, 0, 255, 100);
-    rect(x, y, w, w);
-  };
-
-  this.show = function () {
-    let x = this.i * w;
-    let y = this.j * w;
-    stroke(255);
-    if (this.walls[0]) {
-      line(x, y, x + w, y);
-    }
-    if (this.walls[1]) {
-      line(x + w, y, x + w, y + w);
-    }
-    if (this.walls[2]) {
-      line(x + w, y + w, x, y + w);
-    }
-    if (this.walls[3]) {
-      line(x, y + w, x, y);
-    }
-
-    if (this.visited) {
-      noStroke();
-      fill(255, 0, 255, 100);
-      rect(x, y, w, w);
-    }
-  };
+    if (this.visited)
+      cells[this.row * columns + this.column].style.background = "coral";
+  }
 }
+
+function makeGrid() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      grid.push(new Cell(i, j));
+    }
+  }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      grid[i * columns + j].findNeighbors();
+    }
+  }
+}
+
+const createMazeRowAndColDivs = () => {
+  for (let y = 0; y < rows; y++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    if (y === 0) row.classList.add('top-row');
+    document.body.appendChild(row);
+
+    for (let x = 0; x < columns; x++) {
+      let column = document.createElement('div');
+      column.classList.add('cell');
+      if (x === 0) column.classList.add('column-left');
+      row.appendChild(column);
+    }
+  }
+}
+
+createMazeRowAndColDivs();
+const cells = document.querySelectorAll('.cell');
+makeGrid();
